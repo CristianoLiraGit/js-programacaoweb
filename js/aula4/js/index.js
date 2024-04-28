fc9f5dbdf5d55563badebd2c76142d91a501b862
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2024-04-28 15:27:38 -0300

    Update index.js

--- js/aula4/js/index.js
+++ js/aula4/js/index.js
@@ -1,5 +1,6 @@
 /*
 console.log(5 + 5) → vai fazer a soma
-console.log("5 + 5") → vai fazer a concatenação resultando em 55, pois o js entendeu como string (entre aspas)
+console.log("5 + 5") → vai escrever 5 + 5, pois o js entendeu como string (entre aspas)
 
 */
+
