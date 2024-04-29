4b19b766b81ce841b3ae8273f839c724e5d20aee
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2024-04-28 22:18:16 -0300

    Arquivos da aula de funções
    Aula 8 - Funções (canal Programação Web)

---
+++ js/aula8/index.js
@@ -0,0 +1,43 @@
+/*
+Funções são blocos de códigos feitos para realizar tarefas específicas
+
+É como uma pequena "fábrica" onde você faz uma entrada e ele te dá uma saída
+
+Pode ser encarado como "mini-programas" projetados para fazer uma tarefa que vai contribuir para todo código
+
+Uma função no JavaScript é executada quando "algo" a invoca (chama-a)
+*/
+/*
+function soma(valor1, valor2){
+    return valor1 + valor2;
+}
+
+console.log(soma(10, 10))
+*/
+
+//! Função de cotação do Dolar
+/*function realParaDolar(real, cotacaoDolar){
+    return real * cotacaoDolar;
+}
+
+var valorReal = 50;
+var cotacao = 5.08;
+
+var total = realParaDolar(valorReal, cotacao);
+
+console.log(`O valor em R$ é: ${valorReal}. e o valor em dólar é U$ ${total}.`)*/
+
+function alertaHello(){
+    alert("Olá, Pessoal!");
+}
+
+/*
+function paraCelcius(valorFahrenheit){
+    return (5/9) * (valorFahrenheit - 32);
+}
+
+var x = paraCelcius(77);
+
+alert(`A temperatura é de ${x} graus celcius`.);
+
+*/
